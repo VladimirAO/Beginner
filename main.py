885c06d884d971046a24fd0098cf85b94f22b075
VladimirAO <bee3on@mail.ru>
2024-05-26 01:13:10 +0400

    Add files via upload

---
+++ main.py
@@ -0,0 +1,29 @@
+# 1. Задача "Длина слова"
+a='Длина слова'
+print(len(a))
+
+# 2. Задача "Суммы и разности"
+first=74
+second=56
+summa=first+second
+diff=first-second
+print(summa,diff)
+
+# 3. Задача «Среднее арифметическое».
+first=23
+second=65
+third=15
+mean=(first+second+third)/3
+print (mean)
+
+# 4. Задача «Простые строчки».
+first_string='Вторник'
+second_string='Понедельник'
+print(second_string,', ',first_string)
+
+# 5. Задача «Сложная формула».
+a=12
+b=53
+c=69
+f=((a*b)+(a*c))**3/2
+print(f)
